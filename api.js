8838aaa13eaa238ce63f78fc6afaffbf4c7eb0f4
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-29 23:54:58 -0300

    TS5

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+   res.send('Hello World')
 })
 
-app.listen(8080)
+app.listen(process.env.PORT || 3000)
