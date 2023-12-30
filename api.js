da9199c4b50526aeaeb506c4ac7f5c138abfec82
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-29 23:00:18 -0300

    ts2

--- api.js
+++ api.js
@@ -1,25 +1,8 @@
-const http = require('http');
-const crypto = require('crypto');
-http
-  .createServer((req, res) => {
-    var body = '';
-    req.on('data', function (chunk) {
-      body += chunk;
-    });
-    req.on('end', function () {
-      if (!verifySignature(req, body)) {
-        res.statusCode = 403;
-        res.end("signature didn't match");
-        return;
-      }
-      res.end('ok');
-    });
-  })
-  .listen(3000);
-function verifySignature(req, body) {
-  const signature = crypto
-    .createHmac('sha1', process.env.OAUTH2_SECRET)
-    .update(body)
-    .digest('hex');
-  return signature === req.headers['x-vercel-signature'];
-}
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
