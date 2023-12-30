369a73176c5982bc04c9175d3e1fa509e6163053
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-30 01:10:51 -0300

    ok

--- api.js
+++ api.js
@@ -1,9 +1,20 @@
 
 const express = require('express')
 const app = express()
-const dados = require('./dados.json')
+var fs = require('fs');
 app.use(express.json())
 
-app.get('/kaio', (req, res) => res.dados
+
+
+
+app.get('/kaio', (req, res) =>{
+
+  fs.readFile('./dados.json', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+
+});
 
 app.listen(process.env.PORT || 3000)
