1f7e32d09af986efde3e9e375927e355a87e5e76
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-30 00:08:38 -0300

    OK

--- api.js
+++ api.js
@@ -1,9 +1,8 @@
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/', function (req, res) {
-   res.send('Hello World')
-})
+app.get('/kaio', (req, res) => res.json({'kaio':'oi'}))
 
 app.listen(process.env.PORT || 3000)
