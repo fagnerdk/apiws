3111b73efc7264360ab47ecd36dea502d203612e
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-30 10:33:08 -0300

    apiwsts0

--- api.js
+++ api.js
@@ -4,11 +4,17 @@ const app = express()
 var fs = require('fs');
 
 app.use(express.json())
-function valo(){
-  return  2
+
+var jog0 = 0; // numro 0
+function joguo0(){
+    jog0 +=1
 }
-x = valo()
+setInterval(()=>{
+
+
+x = jog0()
 app.get('/kaio', (req, res) => res.json({'kaio':[1,3,x]}))
 
+},1500)
 
 app.listen(process.env.PORT || 3000)
