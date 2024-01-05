f9f0d1b7330f46eb477360048dfbb21dd48d0d25
fagner <100000573+fagnerdk@users.noreply.github.com>
2024-01-05 19:47:39 -0300

    ok

--- api.js
+++ api.js
@@ -1,36 +1,55 @@
 
-const express = require('express')
+
+import express from "express";
 const app = express()
+
+import parser from "body-parser";
+const urlencodedpase = parser.urlencoded({ extended: false });
+
 app.use(express.json())
-var puppeteer=require( 'puppeteer')
+
 
 console.log('o robo esta ligado ')
 
-const pl = 'vale3'
+const pl = ['vale3','petr4']
 
-async function b() {
-  const browser = await puppeteer.launch({headless: false});
+
+async function b1(a) {
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  await page.goto(`https://www.bing.com/search?q=+dividendo+${pl}&qs=n&form=QBRE&sp=-1&lq=0&pq=+dividendo+${pl}&sc=11-13&sk=&cvid=3849C94B0F9A470DBFF6DD0BFE4D77F3&ghsh=0&ghacc=0&ghpl=`);
   
+   await page.goto(`https://www.bing.com/search?q=+Yield +${pl[0]}&qs=n&form=QBRE&sp=-1&lq=0&pq=+Yield+${pl[0]}&sc=11-13&sk=&cvid=3849C94B0F9A470DBFF6DD0BFE4D77F3&ghsh=0&ghacc=0&ghpl=`);
+ 
    
   const textSelector = await page.waitForSelector(
       '[ class="b_focusTextMedium"]'
     )
     const tex2 = await textSelector.evaluate(el => el.textContent);
     var arry=[...tex2]
-    const x =parseFloat( arry[3]+''+arry[5]+''+arry[6]+''+arry[7])
+    const x =parseFloat( arry[0]+''+arry[2]+''+arry[3]+''+arry[4])
 
   console.log(x)
 
-  app.get('/kaio', (req, res) => res.json({"divdendo":[1,3,x]}))
+  app.get('/kaio', (req, res) => res.json({"divdendo":{'vale3':x}}))
 
- await browser.close(); 
  
-
+  await browser.close()
+ 
+ 
 }
 
-b()
+
+/*app.get("/kaio", function (req, res) {
+  res.end("tudo ok:  " + req.query.name);
+});*/
+
+app.post("/kaio1",urlencodedpase,function (req, res) {
+  res.end( req.body.name);
+  
+})
+
+
+setInterval(()=>{b1();;console.log('ok') },20000)
  
 
 app.listen(process.env.PORT || 3000)
--- but.js
+++ but.js
@@ -1,30 +1,12 @@
 
-var puppeteer=require( 'puppeteer')
+//const express = require('express')
+import express from 'express'
+const app = express()
+function x(){ return 12}
+app.use(express.json())
 
-console.log('o robo esta ligado ')
 
-const pl = 'vale3'
+  app.get('/kaio', (req, res) => res.json({"divdendo":{'petr4':x()}}))
 
-async function b() {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto(`https://www.bing.com/search?q=+dividendo+${pl}&qs=n&form=QBRE&sp=-1&lq=0&pq=+dividendo+${pl}&sc=11-13&sk=&cvid=3849C94B0F9A470DBFF6DD0BFE4D77F3&ghsh=0&ghacc=0&ghpl=`);
-  
-   
-  const textSelector = await page.waitForSelector(
-      '[ class="b_focusTextMedium"]'
-    )
-    const tex2 = await textSelector.evaluate(el => el.textContent);
-    var arry=[...tex2]
-    const x =parseFloat( arry[3]+''+arry[5]+''+arry[6]+''+arry[7])
-
-  console.log(x)
-    
- await browser.close(); 
  
-
-}
-
-b()
- 
-
+app.listen(process.env.PORT || 3000)
