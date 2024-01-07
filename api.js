473889ac6728e3076723370d1adf9b19564d6180
fagner <100000573+fagnerdk@users.noreply.github.com>
2024-01-06 23:57:10 -0300

    apiws

--- api.js
+++ api.js
@@ -12,13 +12,13 @@ import puppeteer from 'puppeteer'
 console.log('o robo esta ligado ')
 
 const pl = ['vale3','petr4']
-
+const posentage = 'Yield'
 
 async function b1(a) {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   
-   await page.goto(`https://www.bing.com/search?q=+Yield +${pl[0]}&qs=n&form=QBRE&sp=-1&lq=0&pq=+Yield+${pl[0]}&sc=11-13&sk=&cvid=3849C94B0F9A470DBFF6DD0BFE4D77F3&ghsh=0&ghacc=0&ghpl=`);
+   await page.goto(`https://www.bing.com/search?q=+${posentage} +${pl[0]}&qs=n&form=QBRE&sp=-1&lq=0&pq=+${posentage}+${pl[0]}&sc=11-13&sk=&cvid=3849C94B0F9A470DBFF6DD0BFE4D77F3&ghsh=0&ghacc=0&ghpl=`);
  
    
   const textSelector = await page.waitForSelector(
@@ -30,12 +30,10 @@ async function b1(a) {
 
   console.log(x)
 
-  app.get('/kaio', (req, res) => res.json({"divdendo":{'vale3':x}}))
+  app.get('/valo', (req, res) => res.json({"divdendo":{'vale3':x,'va':x}})) 
 
- 
-  await browser.close()
- 
- 
+
+ await browser.close()
 }
 setInterval(()=>{b1();;console.log('ok') },20000)
 
@@ -47,8 +45,6 @@ app.post("/kaiop2",urlencodedpase,function (req, res) {
   res.end( req.body.name);
   
 })
-
-
 
  
 
